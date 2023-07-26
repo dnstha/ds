01edff8ba64e78717ac1ccfbb97d82ce2c6067ce
DINESH <dineshshrestha2997@gmail.com>
2023-07-26 17:29:44 +0545

    Simple changes in main page responsiveness and pendulum file added

--- Pendulum/shm.js
+++ Pendulum/shm.js
@@ -12,18 +12,25 @@ function Bob(x, y, radius) {
 
     this.update = function() {
         this.angle += this.velocity;
-        this.x = p.x + Math.cos(this.angle) * 10;
-        this.y = p.y + Math.sin(this.angle) * 200;
+        this.x = p.x + Math.cos(this.angle) * 100;
+        //this.y = p.y + Math.sin(this.angle) * 200;
         this.draw();
     }
 
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        c.stroke();
+        c.fill();
         c.closePath();
+        drawLine(this.x, this.y, avg(p1.x, p2.x), avg(p1.y, p2.y));
     }
 
+}
+
+getDist = (x1, y1, x2, y2) => {
+    let x = x2 - x1;
+    let y = y2 - y1;
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 }
 
 function Point(x, y) {
@@ -31,19 +38,37 @@ function Point(x, y) {
     this.y = y;
 }
 
-let radius, angle;
+function avg() {
+    let x = 0;
+    for(let i = 0; i<arguments.length; i++) {
+        x += arguments[i];
+    }
+    return x/arguments.length;
+}
+
+drawLine = (x1, y1, x2, y2) => {
+    c.beginPath();
+    c.moveTo(x1, y1);
+    c.lineTo(x2, y2);
+    c.strokeStyle = 'black';
+    c.stroke();
+}
+
+let radius, angle, p1, p2, p;
 let b = [];
 function init() {
+    p1 = new Point(canvas.width * 0.25, canvas.height/10);
+    p2 = new Point(canvas.width * 0.75, p1.y);
     b = [];
-    p = new Point(canvas.width/2, canvas.height * 0.5)
-    b.push(new Bob(p.x, p.y, 10));
+    p = new Point(canvas.width/2, canvas.height * 0.9)
+    b.push(new Bob(p.x, p.y, 20));
 }
 
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
     b[0].update();
+    drawLine(p1.x, p1.y, p2.x, p2.y);
 }
-
 init();
 animate();
