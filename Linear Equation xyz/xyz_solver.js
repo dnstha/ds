0384b6cc4e35e8c0072a82c611c474ff117afc9f
DINESH <dineshshrestha2997@gmail.com>
2023-08-05 12:55:18 +0545

    xyz_solver updated and reset button added to each main page

--- Linear Equation xyz/xyz_solver.js
+++ Linear Equation xyz/xyz_solver.js
@@ -2,7 +2,7 @@
 // console.log(M.average(1,2,3,4));
 
 
-let x1, y1, z1, c1, x2, y2, c2, z2, x, y, z, Dx, Dy, Dz, C, s;
+let x1,y1,z1,c1, x2,y2,c2,z2, x3,y3,z3,c3, x,y,z, Dx,Dy,Dz,C, s;
 
 det2 = (a,b, c,d) => {
     return a*d - b*c;
@@ -41,17 +41,49 @@ document.querySelector('button').onclick = () => {
         x = "";
         y = "";
         z = "";
-        if(x1/x2 == y1/y2 && y1/y2 == c1/c2) {
-            s.innerHTML = "*The lines coincide and the system has infinitely many solutions!";
-        }
-        else if(x1/x2 == y1/y2 && y1/y2 != c1/c2) {
-            s.innerHTML = "*The lines are parallel to each other and the system has no solution!";
-        }
-        else if (x1 == 0 && x2 == 0 && y1 != 0 && y2 != 0) {
-            lpta(y1, c1, y2, c2);
-        }
-        else if (y1 == 0 && y2 == 0 && x1 != 0 && x2 != 0) {
-            lpta(x1, c1, x2, c2);
+
+        if(!isAllZero(x1,y1,z1) && !isAllZero(x2,y2,z2) && !isAllZero(x3,y3,z3)) {
+            // When all the planes coincide: INFINITELY MANY SOLUTIONS
+            if(eqRto(x1,x2, y1,y2) && eqRto(x1,x2, z1,z2) && eqRto(z1,z2, c1,c2) && eqRto(x1,x3, y1,y3) && eqRto(x1,x3, z1,z3) && eqRto(z1,z3, c1,c3)) {
+                s.innerHTML = "*The planes coincide and the system has infinitely many solutions!";
+            }
+
+            // When all the planes are parallel to each other without coinciding: NO SOLUTION
+            else if(eqRto(x1,x2, y1,y2) && eqRto(x1,x2, z1,z2) && !eqRto(z1,z2, c1,c2) && eqRto(x1,x3, y1,y3) && eqRto(x1,x3, z1,z3) && !eqRto(z1,z3, c1,c3) && !eqRto(z2,z3, c2,c3)) {
+                s.innerHTML = "*The planes are parallel to each other and the system has no solution!";
+            }
+
+            // When two planes are coincide and the third one is parallel to them: NO SOLUTION
+            else if(eqRto(x1,x2, y1,y2) && eqRto(x1,x2, z1,z2) && !eqRto(z1,z2, c1,c2) && eqRto(x1,x3, y1,y3) && eqRto(x1,x3, z1,z3) && eqRto(z1,z3, c1,c3)) {
+                s.innerHTML = "*The system has no solution! Plane 2 is parallel to the planes 1 and 3! In addition, the planes 1 and 3 coincide in this system!";
+            }
+            else if(eqRto(x1,x2, y1,y2) && eqRto(x1,x2, z1,z2) && eqRto(z1,z2, c1,c2) && eqRto(x1,x3, y1,y3) && eqRto(x1,x3, z1,z3) && !eqRto(z1,z3, c1,c3)) {
+                s.innerHTML = "*The system has no solution! Plane 3 is parallel to the planes 1 and 2! In addition, the planes 1 and 2 coincide in this system!";
+            }
+            else if(eqRto(x1,x2, y1,y2) && eqRto(x1,x2, z1,z2) && !eqRto(z1,z2, c1,c2) && eqRto(x1,x3, y1,y3) && eqRto(x1,x3, z1,z3) && !eqRto(z1,z3, c1,c3) && eqRto(z2,z3, c2,c3)) {
+                s.innerHTML = "*The system has no solution! Plane 1 is parallel to the planes 2 and 3! In addition, the planes 2 and 3 coincide in this system!";
+            }
+
+            // When any of the two planes are parallel to each other without coinciding: NO SOLUTOIN
+            else if(eqRto(x1,x2, y1,y2) && eqRto(x1,x2, z1,z2) && !eqRto(z1,z2, c1,c2)) {
+                s.innerHTML = "*The system has no solution! Planes 1 and 2 are parallel to the each other!";
+            }
+            else if(eqRto(x3,x2, y3,y2) && eqRto(x3,x2, z3,z2) && !eqRto(z3,z2, c3,c2)) {
+                s.innerHTML = "*The system has no solution! Planes 2 and 3 are parallel to the each other!";
+            }
+            else if(eqRto(x1,x3, y1,y3) && eqRto(x1,x3, z1,z3) && !eqRto(z1,z3, c1,c3)) {
+                s.innerHTML = "*The system has no solution! Planes 1 and 3 are parallel to the each other!";
+            }
+            else {
+                s.innerHTML = "*The system has no specific solution but a straight line! Try again!";
+            }
+            // When any of the two planes coincide and the third plane intersects both of them: Solution is an equation of line
+            // else if (x1 == 0 && x2 == 0 && y1 != 0 && y2 != 0) {
+            //     lpta(y1, c1, y2, c2);
+            // }
+            // else if (y1 == 0 && y2 == 0 && x1 != 0 && x2 != 0) {
+            //     lpta(x1, c1, x2, c2);
+            // }
         }
         else{
             alert("Enter cofficient of x or y or z in each equation!");
@@ -61,6 +93,44 @@ document.querySelector('button').onclick = () => {
     document.getElementById("y").value = y;
     document.getElementById("z").value = z;
 };
+
+// Equal Ratio
+eqRto = (a1,b1, a2,b2) => {
+    if(a1*b2 == a2*b1) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+// To check given set of numbers is all zero
+function isAllZero() {
+    let sum = 0;
+    for(let i=0; i<arguments.length; i++) {
+        if(Number(arguments[i]) < 0) {
+            Number(arguments[i]) *= -1;
+        }
+        sum += Number(arguments[i]);
+    }
+    if(sum == 0) {
+        return true;
+    }else{
+        return false;
+    }
+}
+
+//To check if there is any zero in the set
+function isAnyZero() {
+    let result = 1;
+    for(let i=0; i<arguments.length; i++) {
+        if(arguments[i] == 0) {
+            result *= 1;
+        }else{
+            result *= 0;
+        }
+    }
+    return result;
+}
 
 // If the line is parallel to either of axes
 lpta = (x1, c1, x2, c2) => {
