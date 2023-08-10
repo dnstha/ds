9ce9dc04ff5f4bf71b353272a11dcb41980e0aea
DINESH <dineshshrestha2997@gmail.com>
2023-08-10 14:06:21 +0545

    Increasing size of the triangle and sin,cos,tan values feature added to the right triangle animation

--- Right Triangle Animation/animation.js
+++ Right Triangle Animation/animation.js
@@ -10,13 +10,45 @@ window.addEventListener('resize', function() {
     init();
 });
 
-
+let previousx2, previousx3, previousy, factor;
 // addEventListener('click', function(event){
 //     pp = !(pp);
 // });
 
-document.getElementById("plps").addEventListener('click', function(){ // instead you can write window.eventListener
+document.getElementById("plps").addEventListener('click', function(){
     pp = !(pp);
+    if(pp == false) {
+        document.getElementById("plps").style.color = lightColors[1];
+    }else{
+        document.getElementById("plps").style.color = '#cfffee';
+    }
+});
+
+document.getElementById("increase").addEventListener('click', function(){ // instead you can write window.eventListener
+    if(canvas.height - t1.y2 > 100) {
+        previousy = t1.y2;
+        previousx2 = t1.x2;
+        previousx3 = t1.x3; 
+        t1.y2 += 10 * 19/15;
+        t1.y3 = t1.y2;
+        factor = getDist(previousx2,previousy, previousx3, previousy)/getDist(t1.x1,t1.y1, previousx2,previousy);
+        // If statement checks if the new point goes out of the canvas
+        if(factor * getDist(t1.x1,t1.y1, t1.x2,t1.y2) + previousx2 < canvas.width) {
+            t1.x3 = factor * getDist(t1.x1,t1.y1, t1.x2,t1.y2) + previousx2;
+        }
+    }
+});
+
+document.getElementById("decrease").addEventListener('click', function(){ // instead you can write window.eventListener
+    if(t1.y2 - t1.y1 > 60) {
+        previousy = t1.y2;
+        previousx2 = t1.x2;
+        previousx3 = t1.x3; 
+        t1.y2 -= 10 * 19/15;
+        t1.y3 = t1.y2;
+        factor = getDist(previousx2,previousy, previousx3, previousy)/getDist(t1.x1,t1.y1, previousx2,previousy);
+        t1.x3 = factor * getDist(t1.x1,t1.y1, t1.x2,t1.y2) + previousx2;
+    }
 });
 
 document.getElementById("reset").addEventListener('click', function(){ // instead you can write window.eventListener
@@ -63,8 +95,8 @@ function Triangle(x1, y1, x2, y2, x3, y3){
     this.y1 = y1;
     this.y2 = y2;
     this.y3 = y3;
-    this.dx = 3.992 * (Math.random() - 0.5);
-
+    this.dx = 4 * (Math.random() - 0.5);
+    this.dy = (Math.random() - 5) * 4;
     this.update = function(){
         if (pp){
             if(this.x3 + this.dx > innerWidth || this.x3  + this.dx < this.x1){
@@ -72,11 +104,12 @@ function Triangle(x1, y1, x2, y2, x3, y3){
             }
             this.x3 += this.dx;
         }
-        
-
-        AB = Math.round(getDist(this.x1,this.y1, this.x2,this.y2) * 15/19);
-        BC = Math.round(getDist(this.x2,this.y2, this.x3,this.y3) * 15000 / 19) / 1000;
-        CA = Math.round(getDist(this.x1,this.y1, this.x3,this.y3)  * 15000 / 19) / 1000;
+        EAB = getDist(this.x1,this.y1, this.x2,this.y2);
+        EBC = getDist(this.x2,this.y2, this.x3,this.y3);
+        ECA = getDist(this.x1,this.y1, this.x3,this.y3);
+        AB = Math.round(EAB * 15000/19)/1000;
+        BC = Math.round(EBC * 15000 / 19) / 1000;
+        CA = Math.round(ECA  * 15000 / 19) / 1000;
 
         ABC = 90;
         BCA = Math.round(deg(Math.atan(AB/BC)) * 100) / 100;
@@ -132,12 +165,14 @@ function Triangle(x1, y1, x2, y2, x3, y3){
         c.fillText("\u2220A = "+ CAB + "\xB0", this.x1 + 110, this.y2 + 80);
         c.fillText("\u2220C = " + BCA + "\xB0", this.x1 + 250, this.y2 + 80);
         
-        // c.font = 'bold 20px monospace';
-        // c.fillStyle = 'red';
-        // c.fillText("Click anywhere on the screen to Play/Pause the animation.", this.x1 + 20, this.y2 + 120);
+        c.fillStyle = lightColors[1];
+        c.fillText("sinC = AB/CA \u2248 " + AB +"/" + CA + " \u2248 " + Math.round(AB/CA * 1000)/1000, canvas.width - 450, 30);
+        c.fillText("cosC = BC/CA \u2248 " + BC +"/" + CA + " \u2248 " + Math.round(BC/CA * 1000)/1000, canvas.width - 450, 55);
+        c.fillText("tanC = AB/BC \u2248 " + AB +"/" + BC + " \u2248 " + Math.round(AB/BC * 1000)/1000, canvas.width - 450, 80);
     }
 }
 let t1,  AB, BC, CA, ABC, BCA, CAB;
+let EAB, EBC, ECA; // Exact Values of side lengths
 let x1,y1, x2,y2, x3,y3;
 let pp;
 init = () =>{
@@ -149,6 +184,7 @@ init = () =>{
     y3 = y2;    
     t1 = new Triangle(x1,y1, x2,y2, x3,y3);
     pp = true;
+    document.getElementById("plps").style.color = "#cfffee";
 }
 
 function animate(){
