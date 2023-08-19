c889b3dad3d3c3ea1c278be1fc3a3abdb87ee784
DINESH <dineshshrestha2997@gmail.com>
2023-08-19 22:40:49 +0545

    Unit Circle interactivity added

--- UnitCircle/UCscript.js
+++ UnitCircle/UCscript.js
@@ -2,22 +2,46 @@ const canvas = document.getElementById('animate');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const c = canvas.getContext('2d');
-let movingPoint, velocity;
+let movingPoint, angle, optAngle;
 let OA, AP;
 let pp;
+let increment;
+
+
+document.getElementById("plps").addEventListener('click', function(){ // instead you can write window.eventListener
+    pp = !(pp);
+    if(pp == false) {
+        document.getElementById("plps").style.color = lightColors[1];
+    }else{
+        document.getElementById("plps").style.color = '#cfffee';
+    }
+});
+
+document.getElementById("reset").addEventListener('click', function(){ // instead you can write window.eventListener
+    init();
+});
 
 movePoints = () => {
     if(pp == true) {
-        if(velocity > 2*Math.PI) {
-            velocity = 0;
-        }
-        velocity += Math.PI/360;
-        movingPoint.x = Origin.x + UnitCircle.radius * Math.cos(velocity);
-        movingPoint.y = Origin.y + UnitCircle.radius * Math.sin(-velocity);
+        // if(angle > 2*Math.PI) {
+        //     angle = 0;
+        // }
+        angle += increment;
     }
-    connect(UnitCircle.x, UnitCircle.y, movingPoint.x, movingPoint.y); // OP
-    connectColor(UnitCircle.x, UnitCircle.y, movingPoint.x, Origin.y, 'blue'); // OA
-    if(getDist(movingPoint.x, movingPoint.y, movingPoint.x, Origin.y) > 3) {
+    movingPoint.x = Origin.x + UnitCircle.radius * Math.cos(angle);
+    movingPoint.y = Origin.y + UnitCircle.radius * Math.sin(-angle);
+    if(Math.round(deg(angle)*10000)/10000 == 360 || Math.round(deg(angle)*10000)/10000 == 0) {
+        increment *= -1;
+    }
+    OA = Math.round((movingPoint.x - Origin.x)/scale * 100000)/100000;
+    AP = Math.round((Origin.y - movingPoint.y)/scale * 100000)/100000;
+    c.strokeStyle = lightColors[1];
+    c.beginPath();
+    c.arc(Origin.x, Origin.y, 40, 0, -angle, true);
+    c.stroke();
+    connectColor(UnitCircle.x, UnitCircle.y, movingPoint.x, movingPoint.y, 'skyblue'); // OP
+    connectColor(UnitCircle.x, UnitCircle.y, movingPoint.x, Origin.y, 'magenta'); // OA
+    if(getDist(movingPoint.x, movingPoint.y, movingPoint.x, Origin.y) > 1) {
         connectColor(movingPoint.x, movingPoint.y, movingPoint.x, Origin.y, 'yellow'); // AP
     }
     point(movingPoint.x, Origin.y, lightColors[1]);
@@ -25,24 +49,31 @@ movePoints = () => {
     point(movingPoint.x, movingPoint.y, lightColors[1]);
     c.fillStyle = 'lime';
     c.font = 'bold 20px times';
-    c.fillText("P", movingPoint.x, movingPoint.y - 7);
+    c.fillText(`P(${OA},${AP})`, movingPoint.x, movingPoint.y - 7);
     c.fillText("A", movingPoint.x - 7, Origin.y + 17);
 
-    OA = Math.round((movingPoint.x - Origin.x)/scale * 100000)/100000;
-    AP = Math.round((Origin.y - movingPoint.y)/scale * 100000)/100000;
-    c.fillStyle = 'skyblue';
     c.font = 'bold 25px times';
-    c.fillText("OP = 1 Unit", 10, 30);
 
-    c.fillText("OA = " + OA, 10, 55);
+    c.fillStyle = lightColors[1];
+    c.fillText("\u2022 \u2220XOP" + " = " + Math.round(deg(angle)*1000)/1000 + "\xB0", 10, 30);
 
-    c.fillText("AP = " + AP, 10, 80);
+    c.fillStyle = 'skyblue';
+    c.fillText("\u2022 OP = 1 Unit", 10, 55);
+
+    c.fillStyle = 'magenta';
+    c.fillText("\u2022 cos\u2220XOP" + " = OA = " + OA, 10, 80);
+
+    c.fillStyle = 'yellow';
+    c.fillText("\u2022 sin\u2220XOP" + " = AP = " + AP, 10, 105);
 }
 
+optAngle = 0;
 function init() {
     movingPoint = new Plot(PlotX(10), PlotY(0))
-    velocity = 0;
+    angle = optAngle;
+    increment = Math.PI/720;
     pp = true;
+    document.getElementById("plps").style.color = '#cfffee';
 }
 
 function animate() {
@@ -51,9 +82,15 @@ function animate() {
     movePoints();
 }
 
-addEventListener("click", function() {
-    pp = !(pp);
-})
-
 init();
 animate();
+
+document.querySelector('select').addEventListener('change', () => {
+    if(document.getElementById("Angles").value == 'choose') {
+        optAngle = rad(0.5);
+    }else{
+        optAngle = rad(Number(document.getElementById("Angles").value));
+    }
+    init();
+    pp = false;
+});
