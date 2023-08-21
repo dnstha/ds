2b146d6c5b5c800e15be3bca088e1ed6e1998d4f
DINESH <dineshshrestha2997@gmail.com>
2023-08-21 13:04:49 +0545

    Animations and interactivity script added

--- Ellipse/drawEllipse.js
+++ Ellipse/drawEllipse.js
@@ -1,10 +1,15 @@
-let circlePage = document.getElementById('circle');
-circlePage.width = window.innerWidth;
-circlePage.height = window.innerHeight;
+const ellipsePage = document.getElementById('ellipse');
+ellipsePage.width = window.innerWidth;
+ellipsePage.height = window.innerHeight;
 
-let ctx = circlePage.getContext('2d');
-let UnitCircle, p; // Moving point
-let scale = 250;
+const ctx = ellipsePage.getContext('2d');
+let ellipse; // Moving point
+let scale = graphScale;
+let majorAxis, minorAxis;
+let f1, f2; // Focii of the Ellipse
+
+graphColor = 'white';
+drawGraph();
 
 function Plot(x, y) {
     this.x = x;
@@ -12,34 +17,41 @@ function Plot(x, y) {
 }
 
 
-function Circle(x, y, radius) {
+function Ellipse(x, y, a, b) {
     this.x = x;
     this.y = y;
-    this.radius = radius;
+    this.a = a;
+    this.b = b;
+    this.focalLength = Math.sqrt(this.a*this.a - this.b*this.b)
     this.color = lightColors[0];
-    this.velocity = 0;
 
     this.draw = function() {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+        ctx.ellipse(this.x, this.y, this.a, this.b, 0, 0, 2*Math.PI);
         ctx.strokeStyle = this.color;
         ctx.lineWidth = 1.5;
         ctx.stroke();
     }
 }
 
-function drawCircle(){
-    graphColor = 'white';
-    drawGraph();
-    Origin.x = circlePage.width/2;
-    Origin.y = circlePage.height/2;
-    UnitCircle = new Circle(Origin.x, Origin.y, 250);
-    UnitCircle.draw();
+function drawEllipse(){
+    ctx.clearRect(0, 0, ellipsePage.width, ellipsePage.height);
+    Origin.x = ellipsePage.width/2;
+    Origin.y = ellipsePage.height/2;
+    majorAxis = scale * randomInt(5, 10);
+    minorAxis = scale * randomInt(2, 9);
+    while(minorAxis >= majorAxis) {
+        minorAxis = scale * randomInt(2, 9);
+    }
+    ellipse = new Ellipse(Origin.x, Origin.y, majorAxis, minorAxis);
+    f1 = new Plot(Origin.x - ellipse.focalLength, Origin.y);
+    f2 = new Plot(Origin.x + ellipse.focalLength, Origin.y);
+    ellipse.draw();
 }
 
 addEventListener('resize', function() {
-    circlePage.width = window.innerWidth;
-    circlePage.height = window.innerHeight;
-    drawCircle();
+    ellipsePage.width = window.innerWidth;
+    ellipsePage.height = window.innerHeight;
+    drawEllipse();
 });
-drawCircle();
+drawEllipse();
--- Ellipse/ellipseAnimation.js
+++ Ellipse/ellipseAnimation.js
@@ -2,8 +2,9 @@ const canvas = document.getElementById('animate');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const c = canvas.getContext('2d');
-let movingPoint, angle, optAngle;
-let OA, AP;
+let movingPoint;
+let AP, BP;
+let OP = new Plot(0,0);
 let pp;
 let increment;
 
@@ -18,62 +19,54 @@ document.getElementById("plps").addEventListener('click', function(){ // instead
 });
 
 document.getElementById("reset").addEventListener('click', function(){ // instead you can write window.eventListener
+    drawEllipse();
     init();
 });
 
 movePoints = () => {
     if(pp == true) {
-        // if(angle > 2*Math.PI) {
-        //     angle = 0;
-        // }
+        if(angle > 2*Math.PI) {
+            angle = 0;
+        }
         angle += increment;
     }
-    movingPoint.x = Origin.x + UnitCircle.radius * Math.cos(angle);
-    movingPoint.y = Origin.y + UnitCircle.radius * Math.sin(-angle);
-    if(Math.round(deg(angle)*10000)/10000 == 360 || Math.round(deg(angle)*10000)/10000 == 0) {
-        increment *= -1;
-    }
-    OA = Math.round((movingPoint.x - Origin.x)/scale * 100000)/100000;
-    AP = Math.round((Origin.y - movingPoint.y)/scale * 100000)/100000;
-    if(angle > 0) {
-        c.strokeStyle = lightColors[1];
-        c.beginPath();
-        c.arc(Origin.x, Origin.y, 40, 0, -angle, true);
-        c.stroke();    
-    }
-    connectColor(UnitCircle.x, UnitCircle.y, movingPoint.x, movingPoint.y, 'skyblue'); // OP
-    connectColor(UnitCircle.x, UnitCircle.y, movingPoint.x, Origin.y, 'magenta'); // OA
-    if(getDist(movingPoint.x, movingPoint.y, movingPoint.x, Origin.y) > 1) {
-        connectColor(movingPoint.x, movingPoint.y, movingPoint.x, Origin.y, 'yellow'); // AP
-    }
-    point(movingPoint.x, Origin.y, lightColors[1]);
-    point(UnitCircle.x, UnitCircle.y, lightColors[1]);
-    point(movingPoint.x, movingPoint.y, lightColors[1]);
+    movingPoint.x = Origin.x + ellipse.a * Math.cos(angle);
+    movingPoint.y = Origin.y + ellipse.b * Math.sin(-angle);
+    OP.x = Math.round((movingPoint.x - Origin.x)/graphScale * 100000)/100000;
+    OP.y = Math.round((Origin.y - movingPoint.y)/graphScale * 100000)/100000;
+    
+    connectColor(f1.x, f1.y, movingPoint.x, movingPoint.y, 'skyblue'); // OP
+    connectColor(f2.x, f2.y, movingPoint.x, movingPoint.y, 'magenta'); // OA
+
+    point(f1.x, ellipse.y, lightColors[1]);
+    point(f2.x, ellipse.y, lightColors[1]);
+    point(movingPoint.x, movingPoint.y, lightColors[1]); // Moving Point
     c.fillStyle = 'lime';
     c.font = 'bold 20px times';
-    c.fillText(`P(${OA},${AP})`, movingPoint.x, movingPoint.y - 7);
-    c.fillText("A", movingPoint.x - 7, Origin.y + 17);
+    c.fillText(`P(${OP.x}, ${OP.y})`, movingPoint.x, movingPoint.y - 7);
+    c.fillText("A", f1.x - 7, Origin.y + 17);
+    c.fillText("B", f2.x - 7, Origin.y + 17);
 
     c.font = 'bold 25px times';
-
-    c.fillStyle = lightColors[1];
-    c.fillText("\u2022 \u2220XOP" + " = " + Math.round(deg(angle)*1000)/1000 + "\xB0", 15, 30);
-
+    AP = (Math.round(getDist(f1.x,f1.y, movingPoint.x, movingPoint.y) * 1000)/(1000 * graphScale));
+    BP = (Math.round(getDist(f2.x,f2.y, movingPoint.x, movingPoint.y) * 1000)/(1000 * graphScale));
     c.fillStyle = 'skyblue';
-    c.fillText("\u2022 OP = 1 Unit", 15, 55);
+    c.fillText("\u2022 AP = " + AP, 15, 30);
 
     c.fillStyle = 'magenta';
-    c.fillText("\u2022 cos\u2220XOP" + " = OA = " + OA, 15, 80);
+    c.fillText("\u2022 BP = " + BP, 15, 55);
 
-    c.fillStyle = 'yellow';
-    c.fillText("\u2022 sin\u2220XOP" + " = AP = " + AP, 15, 105);
+    c.fillStyle = 'lime';
+    c.fillText("\u2022 AP + BP = " + (AP + BP), 15, 80);
+    c.fillText(`\u2022 A = (${Math.round((f1.x - Origin.x)*1000)/(graphScale*1000)}, 0)` , 15, 105);
+    c.fillText(`\u2022 B = (${Math.round((f2.x - Origin.x)*1000)/(graphScale*1000)}, 0)`, 15, 130);
 }
 
-optAngle = 0;
 function init() {
-    movingPoint = new Plot(PlotX(10), PlotY(0))
-    angle = optAngle;
-    increment = Math.PI/720;
+    drawEllipse();
+    movingPoint = new Plot(Origin.x + ellipse.a, Origin.y)
+    angle = Math.random() * Math.PI;
+    increment = randomSign() * Math.PI/720;
     pp = true;
     document.getElementById("plps").style.color = '#cfffee';
 }
@@ -89,15 +82,4 @@ animate();
 
 document.getElementById("reverse").addEventListener('click', function(){
     increment *= -1;
-});
-
-document.querySelector('select').addEventListener('change', () => {
-    if(document.getElementById("Angles").value == 'choose') {
-        optAngle = rad(0.5);
-    }else{
-        optAngle = rad(Number(document.getElementById("Angles").value));
-    }
-    init();
-    pp = false;
-    document.getElementById("plps").style.color = lightColors[1];
 });
