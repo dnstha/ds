5ae4fa0a5f096cd716e023909b3f8a24753d3743
DINESH <dineshshrestha2997@gmail.com>
2023-08-25 14:15:17 +0545

    Animation angle extended to positive and negative angles

--- UnitCircle/UCscript.js
+++ UnitCircle/UCscript.js
@@ -23,24 +23,26 @@ document.getElementById("reset").addEventListener('click', function(){ // instea
 
 movePoints = () => {
     if(pp == true) {
-        // if(angle > 2*Math.PI) {
-        //     angle = 0;
-        // }
         angle += increment;
     }
     movingPoint.x = Origin.x + UnitCircle.radius * Math.cos(angle);
     movingPoint.y = Origin.y + UnitCircle.radius * Math.sin(-angle);
-    if(Math.round(deg(angle)*10000)/10000 == 360 || Math.round(deg(angle)*10000)/10000 == 0) {
-        increment *= -1;
-    }
+    // if(Math.round(deg(angle)*10000)/10000 == 360 || Math.round(deg(angle)*10000)/10000 == 0) {
+    //     increment *= -1;
+    // }
     OA = Math.round((movingPoint.x - Origin.x)/scale * 100000)/100000;
     AP = Math.round((Origin.y - movingPoint.y)/scale * 100000)/100000;
+    
+    // Angle Drawing
+    c.strokeStyle = lightColors[1];
+    c.beginPath();
     if(angle > 0) {
-        c.strokeStyle = lightColors[1];
-        c.beginPath();
-        c.arc(Origin.x, Origin.y, 40, 0, -angle, true);
-        c.stroke();    
+        c.arc(Origin.x, Origin.y, 40, 0, -angle, true);  
+    }else{
+        c.arc(Origin.x, Origin.y, 40, 0, -angle, false);
     }
+    c.stroke();
+
     connectColor(UnitCircle.x, UnitCircle.y, movingPoint.x, movingPoint.y, 'skyblue'); // OP
     connectColor(UnitCircle.x, UnitCircle.y, movingPoint.x, Origin.y, 'magenta'); // OA
     if(getDist(movingPoint.x, movingPoint.y, movingPoint.x, Origin.y) > 1) {
@@ -73,7 +75,7 @@ optAngle = 0;
 function init() {
     movingPoint = new Plot(PlotX(10), PlotY(0))
     angle = optAngle;
-    increment = Math.PI/720;
+    increment = randomSign() * Math.PI/720;
     pp = true;
     document.getElementById("plps").style.color = '#cfffee';
 }
@@ -93,7 +95,7 @@ document.getElementById("reverse").addEventListener('click', function(){
 
 document.querySelector('select').addEventListener('change', () => {
     if(document.getElementById("Angles").value == 'choose') {
-        optAngle = rad(0.5);
+        optAngle = 0;
     }else{
         optAngle = rad(Number(document.getElementById("Angles").value));
     }
