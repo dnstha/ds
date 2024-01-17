83e2045cebb99e9964d70f1e213e58f7dcc7d0a3
DINESH <dineshshrestha2997@gmail.com>
2024-01-17 20:27:28 +0545

    Webpage updated with improved features and responsiveness. New icon added with bolder strokes

---
+++ resp.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector('#navBtn');
+const icon = document.querySelector('#navMenu');
+
+btn.addEventListener('click', ()=>{
+    if(btn.checked == true){
+        icon.innerHTML = `&#128938`;
+    }else{
+        icon.innerHTML = `
+            <div class="hamburger"></div>
+            <div class="hamburger"></div>
+            <div class="hamburger"></div>        
+        `;
+    }
+});
+
+
+// document.querySelectorAll('a[href=""#"]').forEach(elem.addEventListener('click' , (event)=> {
+//     event.preventDefault();
+//     window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth'
+//     });
+// }));
