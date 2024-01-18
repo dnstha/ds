3e5f50e1c6b03de1042985eb70275431c8384889
DINESH <dineshshrestha2997@gmail.com>
2024-01-18 20:35:16 +0545

    Hamburger menu customized with border radius, and additional changes to the index.html, resp.js, and style.css

--- resp.js
+++ resp.js
@@ -14,10 +14,14 @@ btn.addEventListener('click', ()=>{
 });
 
 
-// document.querySelectorAll('a[href=""#"]').forEach(elem.addEventListener('click' , (event)=> {
-//     event.preventDefault();
-//     window.scrollTo({
-//         top: 0,
-//         behavior: 'smooth'
-//     });
-// }));
+// addEventListener('load', function(){
+//     if(btn.checked == true){
+//         icon.innerHTML = `&#x2715`;
+//     }else{
+//         icon.innerHTML = `
+//             <div class="hamburger"></div>
+//             <div class="hamburger"></div>
+//             <div class="hamburger"></div>        
+//         `;
+//     }
+// });
