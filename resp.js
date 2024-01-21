e23102da97c34919439db161467ff354594812c8
DINESH <dineshshrestha2997@gmail.com>
2024-01-21 17:44:22 +0545

    Preloader added and hamburger icon changing issue fixed

--- resp.js
+++ resp.js
@@ -1,7 +1,34 @@
-const btn = document.querySelector('#navBtn');
-const icon = document.querySelector('#navMenu');
+window.addEventListener('load', function(){
+    const btn = document.querySelector('#navBtn');
+    const icon = document.querySelector('#navMenu');
+    const loader = document.querySelector('.preloader');
+    const header = document.querySelector('h1');
+    const intro = document.querySelector('p');
+    header.style.fontSize = 0;
+    intro.style.fontSize = 0;
 
-btn.addEventListener('click', ()=>{
+    loader.classList.add("preloader-hidden");
+    loader.addEventListener('transitionend', function(){
+        document.body.removeChild(loader);
+        document.querySelector('h1').classList.add('h-animation');
+        document.querySelector('p').classList.add('intro-animation');
+        header.style.fontSize = '2.5rem';
+        intro.style.fontSize = '1.5rem';
+    });
+
+    btn.addEventListener('click', ()=>{
+        if(btn.checked){
+            icon.innerHTML = `&#x2715;` || `✕` || `&#x2573;`;
+        }else{
+            icon.innerHTML = `
+                <div class="hamburger"></div>
+                <div class="hamburger"></div>
+                <div class="hamburger"></div>     
+            `;
+        }
+    });
+
+
     if(btn.checked){
         icon.innerHTML = `&#x2715;` || `✕` || `&#x2573;`;
     }else{
@@ -12,14 +39,3 @@ btn.addEventListener('click', ()=>{
         `;
     }
 });
-
-
-if(btn.checked){
-    icon.innerHTML = `&#x2715;` || `✕` || `&#x2573;`;
-}else{
-    icon.innerHTML = `
-        <div class="hamburger"></div>
-        <div class="hamburger"></div>
-        <div class="hamburger"></div>        
-    `;
-}
