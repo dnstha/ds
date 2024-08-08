7b48394d7da951b176d99263660a0ae6b1149ed3
DINESH <dineshshrestha2997@gmail.com>
2024-08-08 18:27:44 +0545

    Code improvements in Math1.js file

--- DSA_UtilityFunctions/MathsFunctions/Math1.js
+++ DSA_UtilityFunctions/MathsFunctions/Math1.js
@@ -1,42 +1,48 @@
-getDist = (x1, y1, x2, y2) => {
+// Calculates distance between points (x1, y1) and (x2, y2)
+const getDist = (x1, y1, x2, y2) => {
     let x = x2 - x1;
     let y = y2 - y1;
     return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
 }
 
 //Determinant of 2x2 matrix
-det2 = (a,b, c,d) => {
+const det2 = (a,b, c,d) => {
     return a*d - b*c;
 }
 
 //Determinant of 3x3 matrix
-det3 = (a,b,c, d,e,f, g,h,i) => {
+const det3 = (a,b,c, d,e,f, g,h,i) => {
     return (a*det2(e,f,h,i) - b*det2(d,f,g,i) + c*det2(d,e,g,h));
 }
 
-deg = (x) =>{
+// Converts given value of x in radians to degrees
+const deg = (x) =>{
     return x*180/Math.PI;
 }
 
-rad = (x) =>{
+// Converts given value of x in degrees to radians
+const rad = (x) =>{
     return x*Math.PI/180;
 }
 
-randomInt = (min, max) => {
+const randomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 // Generates any random number in given range including decimal numbers
 const randomNum = (min, max) => Math.random() * (max - min) + min;
 
-dotProduct = (a1,b1, a2,b2) => {
+// Returns dot product of vertors (a1, b1) and (a2, b2)
+const dotProduct = (a1,b1, a2,b2) => {
     return a1*a2 + b1*b2;
 }
 
-vctr = (x1, x2) => {
+// Returns resolved vectors in an axis from given value x1 and x2
+const vctr = (x1, x2) => {
     return x2-x1;
 }
 
+// Returns average of entered numbers
 function average(...arg) {
     let avg = 0;
     let n = arg.length;
@@ -67,11 +73,13 @@ roundUp = (number, place) => {
     return Math.round(number * place)/place;
 }
 
-randomSign = () => {
+// Returns a random signed 1, i.e. +1 or -1
+const randomSign = () => {
     return Math.pow(-1, randomInt(2,3));
 }
 
-slope = (V1) => {
+// Returns slope of given vector with respect to the positive x-axis
+const slope = (V1) => {
     let A = Math.acos(dotProduct(V1.x, V1.y, 1, 0)/getDist(p.x, p.y, fixed[1].x, fixed[1].y));
     if(V1.y <= 0) {
         A = Math.PI * 2 - A;
@@ -80,7 +88,7 @@ slope = (V1) => {
 }
 
 // Equal Ratio to check if two given ratios are equal a1/b1 and a2/b2
-eqRto = (a1,b1, a2,b2) => {
+const eqRto = (a1,b1, a2,b2) => {
     if(a1*b2 == a2*b1) {
         return true;
     }else{
@@ -118,7 +126,7 @@ function isAnyZero() {
 }
 
 // Modulus function
-modulus = (x) => {
+const modulus = (x) => {
     if(x < 0) {
         return -x;
     }else{
@@ -127,7 +135,7 @@ modulus = (x) => {
 }
 
 // Inverse cos having range from 0 to 2PI
-invCos = (value, y) => {
+const invCos = (value, y) => {
     if(y>=0) {
         return Math.acos(value);
     }else if(y<0){
@@ -139,7 +147,7 @@ invCos = (value, y) => {
 
 
 // Returns true, or 1, if all the fields are full otherwise returns false, or 0.
-filled = (text) => {
+const filled = (text) => {
     let result = 1;
     document.querySelectorAll(text).forEach(element => {
         if(element.value == '' || element.value == 'null') {
